2fe0500592293de472ecc0b39df59b98d092a5cb
priyanshiagarw17 <priyanshi.agarwal_cs22@gla.ac.in>
2025-03-27 11:43:07 +0530

    rename feature for easy use

--- analysis/DataAnalyzer.py
+++ analysis/DataAnalyzer.py
@@ -6,6 +6,7 @@ class Analyzer:
     def __init__(self, file_path):
 
         self.df = pd.read_csv(file_path)
+        print(self.df)
 
     def show_basic_info(self):
 
@@ -19,4 +20,18 @@ class Analyzer:
         print(self.df.describe())
         print("\nMissing values:")
         print(self.df.isnull().sum())
+
+    def  rename(self):
+        self.df.rename(columns={
+            "Do you have Depression?": "Depression",
+            "Do you have Anxiety?": "Anxiety",
+            "What is your CGPA?": "CGPA",
+            "marital status": "MaritalStatus",
+            "Do you have Panic attack?" : "Panicattack",
+            "Did you seek any specialist for a treatment?" : "Specialist",
+            "Choose your gender" : "Gender",
+            "What is your course?" : "Course",
+            "Your current year of Study" : "Year"
+        }, inplace=True)
+        print(self.df)
 
--- analysis/main.py
+++ analysis/main.py
@@ -2,4 +2,6 @@ from DataAnalyzer import Analyzer
 
 Analysis = Analyzer(r"C:\Users\user\OneDrive\Desktop\Student Mental health.csv")
 
+Analysis.__init__(file_path=r"C:\Users\user\OneDrive\Desktop\Student Mental health.csv")
 Analysis.show_basic_info()
+Analysis.rename()
